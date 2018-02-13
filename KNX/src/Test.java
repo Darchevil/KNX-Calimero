f6a9e1d39ee7ff8777a6c8d36273516199e2a7b7
Yoann d'Erneville <yderneville@gmail.com>
2018-02-13 18:02:13 +0100

    Toto

--- KNX/src/Test.java
+++ KNX/src/Test.java
@@ -3,25 +3,31 @@ import java.net.InetSocketAddress;
 import tuwien.auto.calimero.GroupAddress;
 import tuwien.auto.calimero.IndividualAddress;
 import tuwien.auto.calimero.KNXException;
+import tuwien.auto.calimero.knxnetip.Discoverer;
 import tuwien.auto.calimero.link.KNXNetworkLinkIP;
 import tuwien.auto.calimero.link.medium.KNXMediumSettings;
 import tuwien.auto.calimero.process.ProcessCommunicator;
 import tuwien.auto.calimero.process.ProcessCommunicatorImpl;
 
 public class Test {
-	public static void main(String args[]) throws KNXException, InterruptedException{
-		
-		InetSocketAddress localaddr = InetSocketAddress.createUnresolved("148.60.172.20", 3000);
-		InetSocketAddress remoteEP = InetSocketAddress.createUnresolved("192.168.1.10", 3000);
-		IndividualAddress IA = new IndividualAddress(1,2,1);
+	public static void main(String args[]) throws KNXException, InterruptedException {
+			connection();	
+	
+	}
+	public static void connection() throws KNXException, InterruptedException
+	{
+		InetSocketAddress localaddr = InetSocketAddress.createUnresolved("148.60.172.20", 3671);
+		InetSocketAddress remoteEP = InetSocketAddress.createUnresolved("192.168.0.10", 3671);
+		IndividualAddress IA = new IndividualAddress(0, 0, 0);
 		KNXMediumSettings settings = null;
 		settings.setDeviceAddress(IA);
 		
 			KNXNetworkLinkIP netLinkIP = KNXNetworkLinkIP.newTunnelingLink(localaddr, remoteEP, false,settings);
 			ProcessCommunicator pc = new ProcessCommunicatorImpl(netLinkIP);
 			
-			float Temp = pc.readFloat(new GroupAddress("0/1/0"));
-		//Lampes
+			double temp = pc.readFloat(new GroupAddress("0/1/0"),false);
+			
+			//Lampes
 			pc.write(new GroupAddress("0/0/1"), true);
 			pc.write(new GroupAddress("0/0/2"), true);
 			pc.write(new GroupAddress("0/0/3"), true);
@@ -44,7 +50,6 @@ public class Test {
 			
 			Discoverer disc = new Discoverer(0, true);
 			disc.startSearch(100, true);
-			
-	
+		
 	}
 }
